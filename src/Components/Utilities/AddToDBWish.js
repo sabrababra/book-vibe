f3c813527f4df706ab6f559732e6946f35dd7024
Sabrina Yemin Prome <sabrinayesminprome@gmail.com>
2025-10-31 16:17:49 +0600

    Fixed wishList bug

--- src/Components/Utilities/AddToDBWish.js
+++ src/Components/Utilities/AddToDBWish.js
@@ -1,5 +1,6 @@
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
+import { getStoreBooks } from "./AddToDB";
 
 const getStoreBooksWishList = () => {
   const storeBookSTR = localStorage.getItem("wishList");
@@ -15,15 +16,19 @@ const MySwal = withReactContent(Swal);
 
 const addToStoreDBWishList = (id) => {
   const storeBookData = getStoreBooksWishList();
+  const readListData=getStoreBooks();
   if (storeBookData.includes(id)) {
     alert("This book is already added!!");
+  }else if(readListData.includes(id)){
+        Swal.fire("Already in Read List!");
+
   } else {
     storeBookData.push(id);
     const data = JSON.stringify(storeBookData);
     localStorage.setItem("wishList", data);
     Swal.fire({
       title: "Good job!",
-      text: "Added Read List Successfully!",
+      text: "Added Wish List Successfully!",
       icon: "success",
     });
   }
